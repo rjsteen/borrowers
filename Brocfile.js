2afb4d20ed4c520ed65fd922dab964101aebec28
Richard Steen <richard.steen@activenetwork.com>
2015-03-31 16:26:41 -0500

    page 54

--- Brocfile.js
+++ Brocfile.js
@@ -4,8 +4,19 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('bower_components/picnic/releases/picnic.min.css');
-
+app.import('bower_components/picnic/releases/v2.min.css');
+app.import('vendor/fontello/fontello.css');
+app.import('vendor/fontello/font/fontello.ttf', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.eot', {
+  destDir: 'font'
+}); app.import('vendor/fontello/font/fontello.svg', {
+  destDir: 'font'
+});
+app.import('vendor/fontello/font/fontello.woff', {
+  destDir: 'font'
+});
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
