1b454d4051cc670e2723a0a9a7ff59710a4a3f67
Richard Steen <richard.steen@activenetwork.com>
2015-04-28 10:59:03 -0500

    page 129

---
+++ server/mocks/articles.js
@@ -0,0 +1,48 @@
+module.exports = function(app) {
+  var express = require('express');
+  var articlesRouter = express.Router();
+
+  articlesRouter.get('/', function(req, res) {
+    res.send({
+      'articles': []
+    });
+  });
+  articlesRouter.get('/74', function(req, res) {
+    res.send({
+      "article":{
+        "id":74,
+        "created_at": "2014-11-03T21:30:47.869Z",
+        "description": "foo",
+        "state": "borrowed",
+        "notes": "bar",
+        "friend_id": 153
+      }
+    });
+  });
+
+  articlesRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  articlesRouter.get('/:id', function(req, res) {
+    res.send({
+      'articles': {
+        id: req.params.id
+      }
+    });
+  });
+
+  articlesRouter.put('/:id', function(req, res) {
+    res.send({
+      'articles': {
+        id: req.params.id
+      }
+    });
+  });
+
+  articlesRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/articles', articlesRouter);
+};
