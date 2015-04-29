fb6f93a99e93bf53116aac5b75373dd26540af25
Richard Steen <richard.steen@activenetwork.com>
2015-04-29 08:13:16 -0500

    fin!

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,11 @@
 import Ember from 'ember'; 
 import request from 'ic-ajax';
-export default Ember.Route.extend({ 
+import config from '../config/environment';
+
+export default Ember.Route.extend({
   model: function() {
-    return request('/api/friends').then(function(data){
+    var host = config.host || '';
+    return request(host + '/api/friends').then(function(data){
       return {
         friendsCount: data.friends.length
       };
